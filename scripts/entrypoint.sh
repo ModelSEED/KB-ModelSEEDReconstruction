246a07e9e158ddbe57774f1a7465a2d79fe4d5fe
Christopher Henry <chenry@mcs.anl.gov>
2023-06-19 00:56:30 -0500

    Removing data files and adding reference data handling

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -17,6 +17,15 @@ elif [ "${1}" = "async" ] ; then
   sh ./scripts/run_async.sh
 elif [ "${1}" = "init" ] ; then
   echo "Initialize module"
+  cd /data
+  curl -L https://bioseed.mcs.anl.gov/~fliu/modelseedpy/knn_ACNP_RAST_full_01_17_2023_features.json > knn_ACNP_RAST_full_01_17_2023_features.json
+  curl -L https://bioseed.mcs.anl.gov/~fliu/modelseedpy/knn_ACNP_RAST_full_01_17_2023.pickle > knn_ACNP_RAST_full_01_17_2023.pickle 
+ 
+  if [ -f knn_ACNP_RAST_full_01_17_2023_features.json ] && [ -f knn_ACNP_RAST_full_01_17_2023.pickle ]; then
+  	touch __READY__
+  else
+    echo "Init failed"
+  fi
 elif [ "${1}" = "bash" ] ; then
   bash
 elif [ "${1}" = "report" ] ; then
